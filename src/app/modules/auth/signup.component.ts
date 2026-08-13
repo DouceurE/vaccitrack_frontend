081d1c159720ba29faa301d3dac139b4285da7d2
DouceurE <maremeba857@gmail.com>
2026-08-13 15:42:10 +0200

    fix: deplacement du dossier environments dans src

--- src/app/modules/auth/signup.component.ts
+++ src/app/modules/auth/signup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-signup',
